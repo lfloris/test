70a307c9c415b4e039026ce9203f52327140e6a4
Luca Floris <luca.floris@uk.ibm.com@macbook-pro.home>
2018-03-24 18:03:01 +0000

    updated app

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 def hello():
 
     html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
+           "<b>Hostname:</b> {hostname}<br/> \
+           <b>I have just updated this app with a CICD pipeline!</b> "
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
-    return 'My updated hostname is %s'
-    return 'I have just updated this app with a CICD pipeline!'
+    return 'My hostname is %s'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
